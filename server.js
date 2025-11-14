629a5b6b8e32902664c69d34aa5bfdf5c8a19b4f
FApew <112774976+FApew@users.noreply.github.com>
2025-11-14 20:44:04 +0100

    fixtest1

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const __dirname = path.dirname(__filename)
 const PORT = process.env.PORT || 3500
 
 const app = express()
-app.use(express.static(path.join(__dirname, "src")))
+app.use("/src", express.static(path.join(__dirname, "src")))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/index.html"))
 })
